3629569564537d8c83f2866a26464f3a937b6288
772vjrvj <42423955+772vjrvj@users.noreply.github.com>
2020-07-01 20:06:10 +0900

    1 1

--- src/index.js
+++ src/index.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Step5AddInverseDataFlow from './MAIN CONCEPTS/12. Thinking In React/Step 5 Add Inverse Data Flow';
+import React from "react";
+import ReactDOM from "react-dom";
+import Step5AddInverseDataFlow from "./MAIN CONCEPTS/12. Thinking In React/Step 5 Add Inverse Data Flow";
 
-
-
-  const PRODUCTS = [
-    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-  ];
-
-
+const PRODUCTS = [
+  {
+    category: "Sporting Goods",
+    price: "$49.99",
+    stocked: true,
+    name: "Football"
+  },
+  {
+    category: "Sporting Goods",
+    price: "$9.99",
+    stocked: true,
+    name: "Baseball"
+  },
+  {
+    category: "Sporting Goods",
+    price: "$29.99",
+    stocked: false,
+    name: "Basketball"
+  },
+  {
+    category: "Electronics",
+    price: "$99.99",
+    stocked: true,
+    name: "iPod Touch"
+  },
+  {
+    category: "Electronics",
+    price: "$399.99",
+    stocked: false,
+    name: "iPhone 5"
+  },
+  { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" }
+];
 
 ReactDOM.render(
   <React.StrictMode>
     <Step5AddInverseDataFlow products={PRODUCTS} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
